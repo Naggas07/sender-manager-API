5cb89a63c82ab5dc5dcae753ce51c33ec48d018b
Naggas07 <rober.araya.gil@gmail.com>
2020-06-02 20:57:40 +0200

    created subProduct model

---
+++ models/subProduct.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { camelCase } = require("../config/normalize/normalize");
+
+const subProductSchema = new Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      uppercase: true,
+    },
+    description: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = doc.id;
+        ret.name = camelCase(doc.name);
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+const SubProduct = mongoose.model("SubProduct", subProductSchema);
+
+module.exports = SubProduct;
